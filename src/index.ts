dccd11697f8ab8f273dda5b77c68bef078b70e19
MarcAT92 <94722932+MarcAT92@users.noreply.github.com>
2025-03-10 13:29:55 -0400

    Create app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Bun Dev, I'm going to build a RESTFULL API").get('/post/:id', ({params: {id}}) => {return {id: id, title: 'Learn Bun'}})
+.post('/post', (body) => {return body})
 .listen(3000);
 
 console.log(
