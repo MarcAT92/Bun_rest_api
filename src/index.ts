83f08ecec1c80adf9096f9a8fc915047b51b35a4
MarcAT92 <94722932+MarcAT92@users.noreply.github.com>
2025-03-11 09:47:16 -0400

    routes and json

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
 import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Bun Dev, I'm going to build a RESTFULL API").get('/post/:id', ({params: {id}}) => {return {id: id, title: 'Learn Bun'}})
-.post('/post', (body) => {return body})
+.post('/post', ({body, set}) => {
+  set.status = 201
+  return body
+})
+.get('/track/*', () => {return "Track Route"})
+.get('/tracks', () => {
+  // return new Response(JSON.stringify({
+  //   tracks: [
+  //     {
+  //       id: 1,
+  //       title: 'Dancing Feet',
+  //     },
+  //     {
+  //       id: 2,
+  //       title: 'Never Gonna Give You Up'
+  //     }
+  //   ]
+  // }), {
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  // })
+  return {"tracks": [
+      {
+         id: 1,
+         title: 'Dancing Feet',
+       },
+       {
+         id: 2,
+         title: 'Never Gonna Give You Up'
+       }
+     ]
+  }
+})
 .listen(3000);
 
 console.log(
